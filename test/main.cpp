2421a1d778ca12d448535485013c1b77e9c81a58
rkluzinski <ryankluzinski@gmail.com>
2020-01-07 21:11:07 -0700

    Update test runner

--- test/main.cpp
+++ test/main.cpp
@@ -4,61 +4,44 @@
 
 #include "../cpu/cpu.h"
 
-class TestException : std::exception {
-    std::string _message;
-
-public:
-    TestException(): _message("") {}
-    TestException(const char *message): _message(message) {}
-    TestException(std::string message): _message(message) {}
-
-    const char *what() {
-        return _message.c_str();
-    }
+// assembled test BDOS file
+const std::array<uint8_t, 0x22> bdos = {
+    0x76, 0x00, 0x00, 0x00, 0x00, 0x3e, 0x02, 0xb9,
+    0xca, 0x14, 0x00, 0x3e, 0x09, 0xb9, 0xca, 0x18,
+    0x00, 0xc3, 0x00, 0x00, 0x7b, 0xd3, 0x00, 0xc9,
+    0x1a, 0xfe, 0x24, 0xc8, 0xd3, 0x00, 0x13, 0xc3,
+    0x18, 0x00
 };
 
-void loadBinary(std::string path, uint8_t *memory, std::size_t length) {
-	std::ifstream binaryFile;
-    binaryFile.open(path, std::ios::in | std::ios::binary);
-
-    if (binaryFile.fail()) {
-        throw TestException("File not found: " + path);
-    }
-
-    binaryFile.read((char *) memory, length);
-
-    if ((std::size_t) binaryFile.gcount() == length) {
-        throw TestException("Binary exceeds Intel8080 memory limit");
-    }
-}
-
-void portOut(uint8_t port, uint8_t byte) {
-    if (port != 0) {
-        throw TestException("Invalid I/O port write");
-    }
-
-    std::cout << (char) byte;
-}
-
 int main(int argc, char **argv) {
+    // check arguments
     if (argc != 2) {
         std::cout << "usage: test8080 [COM]" << std::endl;
-        return 0;
+        return 1;
     }
 
-	Intel8080 cpu;
+    // open test file
+    std::ifstream test(argv[1], std::ios::in | std::ios::binary);
+    if (test.fail()) {
+        return 1;
+    }
+
+    Intel8080 cpu;
     cpu.program_counter = 0x100;
-    cpu.out = portOut;
     
-    try {
-        loadBinary("../roms/BDOS", cpu.memory.data(), cpu.memory.size());
-        loadBinary(argv[1], cpu.memory.data() + 0x100, cpu.memory.size() - 0x100);
-        
-        std::size_t cycles = cpu.execute();
-        std::cout << "\ncycles executed: " << cycles << std::endl;
-    }
-    catch (TestException &e) {
-        std::cerr << "TestException: " << e.what() << std::endl;
-    }
+    // CPU out port callback
+    cpu.out = [](uint8_t port, uint8_t byte) {
+        if (port == 0) {
+            std::cout << byte;
+        }
+    };
+
+    // load BDOS test file into RAM
+    std::copy(bdos.begin(), bdos.end(), cpu.memory.begin());
+	test.read((char *) cpu.memory.data() + 0x100, cpu.memory.size() - 0x100);
+    
+    // execute program
+    auto cycles = cpu.execute();
+    std::cout << std::endl << "Cycles executed: " << cycles << std::endl;
 	return 0;
 }
